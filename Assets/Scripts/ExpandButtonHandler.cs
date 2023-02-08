62a8f5f2593bd86a6fc698f26f0a46a23668de1c
DanSchmid <dasc5174@colorado.edu>
2023-02-07 17:48:18 -0700

    Started work on a more versatile sidebar menu.  Has tabs to navigate sub-pages, and a list of toggles with expandable sections for selecting celestial bodies to simulate.

---
+++ Assets/Scripts/ExpandButtonHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpandButtonHandler : MonoBehaviour
+{
+    public Canvas canvas;
+    public Text buttontext;
+    private bool isExpanded = false;
+    
+    public void EnableCanvas()  //toggles the expansion of the canvas area, and the text of the button. The canvas area must be a sibling of the expandable header, below it in hierarchy
+    {
+        if(isExpanded)
+        {
+            buttontext.text = "+";
+        }
+        else
+        {
+            buttontext.text = "-";  //Need to improve the positioning of this when the text changes. It's not centered.
+        }
+        canvas.gameObject.SetActive(!isExpanded);
+        isExpanded = !isExpanded;
+    }
+}
